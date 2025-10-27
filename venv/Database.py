61702506f86d9d08779ede74d704c59f2c381438
Carlos Bernal <dexterthefirst112@gmail.com>
2025-10-27 02:20:40 -0500

    Initial commid of Python Code, warning, may or may not work

---
+++ venv/Database.py
@@ -0,0 +1,9 @@
+# db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://user:pass@localhost:3306/fintrack")
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
