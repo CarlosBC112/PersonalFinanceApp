de1076aa07fb7113e8879254514d4286ec1e6ceb
Carlos Bernal <dexterthefirst112@gmail.com>
2025-11-17 09:47:01 -0600

    Clean up obsolete files - removed redundant scripts and SQL files

--- DatabaseMySQL/category.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO categories (name, description) VALUES
-  ('Utilities', 'Electric, water, phone, internet, etc.'),
-  ('Groceries', 'Supermarkets, food stores'),
-  ('Savings', 'Transfers to savings/investments'),
-  ('Social', 'Restaurants, bars, gifts, subscriptions'),
-  ('Transportation', 'Fuel, rideshare, public transit, maintenance'),
-  ('Housing', 'Rent, mortgage, insurance, property tax');
